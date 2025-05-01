b7c80fb490b603fd4cd620b40adb7d8125ec9ff8
Jeffrey Rubio <rubiojeff07@gmail.com>
2025-04-30 20:18:03 -0700

    commit

--- DatingAppMobile/App.js
+++ DatingAppMobile/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Import your screens
+import AuthOptionsScreen from './screens/AuthOptionsScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
 const Stack = createStackNavigator();
@@ -9,16 +13,34 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
-        <Stack.Screen 
-          name="Register" 
-          component={RegisterScreen} 
-          options={{ title: 'Register' }} 
+      <Stack.Navigator initialRouteName="AuthOptions">
+
+        {/* Screen for choosing between Signup and Login */}
+        <Stack.Screen
+          name="AuthOptions"
+          component={AuthOptionsScreen}
+          options={{ title: 'Welcome' }}
         />
-        <Stack.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
-          options={{ title: 'Profile' }} 
+
+        {/* Signup screen */}
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: 'Sign Up' }}
+        />
+
+        {/* Login screen */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Log In' }}
+        />
+
+        {/* Protected profile screen */}
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: 'Your Profile' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
